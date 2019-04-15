6f3e2e890166319a57eaf3e8eb2df5b8396e74fa
Phaninder Kumar <phaninderkumar@Phaninders-MacBook-Pro.local>
2019-04-15 22:39:15 +0530

    Added post clone script

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,14 +1,17 @@
 import UIKit
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
     static var entries: FileEntryStore = FileEntryStore()
-    
+    //9cbea2f7-81fc-424b-952c-354718a60b84
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        MSAppCenter.start("9cbea2f7-81fc-424b-952c-354718a60b84", withServices: [MSDistribute.self, MSCrashes.self])
         return true
     }
 
